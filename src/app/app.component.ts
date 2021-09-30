dbe3fde88ce21f366bff75d759e4abc2121b831d
shady-azouz <shady.azouz@gmail.com>
2021-09-30 12:46:34 +0200

    mod: alter navbar and footer visibility according to loggedIn status not route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { LoginService } from './shared/services/login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [LoginService]
 })
 export class AppComponent{
   title = 'Azouz-Movie-Database-App';
 
-  constructor(public router: Router) {}
+  isLoggedIn() {
+    return localStorage.getItem('loggedIn') === 'true';
+  }
 }
